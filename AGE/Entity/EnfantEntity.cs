167df004cf62b421f28c0117219851960a33e33a
Rachid <rezi207@gmail.com>
2023-08-08 21:42:10 +0200

    ajour de la class  enfantentity

---
+++ AGE/Entity/EnfantEntity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AGE.Entity
+{
+    internal class EnfantEntity
+    {
+        public SqlConnection MyConnecion = new SqlConnection("Data Source=DESKTOP-1K0GE9A\\SQLEXPRESS;Initial Catalog=GestioEnfant;Integrated Security=True");
+        public string RequetteSQl;
+
+        public void GetListEnfant(ListBox ListeBoxEnfant)
+        {
+            RequetteSQl = "Select * From Membre";
+            SqlCommand MyCommand = new SqlCommand(RequetteSQl, MyConnecion);
+            
+
+            try
+            {
+                MyConnecion.Open();
+                MyCommand.ExecuteNonQuery();
+                SqlDataReader MyDataReader = MyCommand.ExecuteReader();
+                while (MyDataReader.Read())
+                {
+                    ListeBoxEnfant.Items.Add(MyDataReader["Nom"]);
+                }
+
+            }
+            catch (Exception ex){
+                MessageBox.Show(ex.ToString(), "MyProgram", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            finally
+            {
+                if (MyConnecion.State == ConnectionState.Open)
+                {
+                    MyConnecion.Close();
+                }
+            }
+        }
+    }
+}
